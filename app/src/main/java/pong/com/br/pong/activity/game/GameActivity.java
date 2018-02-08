cb62e85f7acfb9303918fa0a56d04e657366ac71
leoallvez <leoallvez@hotmail.com>
2018-02-08 00:28:27 -0200

    Paint

--- app/src/main/java/pong/com/br/pong/activity/game/GameActivity.java
+++ app/src/main/java/pong/com/br/pong/activity/game/GameActivity.java
@@ -1,15 +1,39 @@
 package pong.com.br.pong.activity.game;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-import pong.com.br.pong.R;
+import pong.com.br.pong.activity.sg.SGActivity;
+import pong.com.br.pong.model.sg.SGPreferences;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends SGActivity {
+
+    public static final String TAG = "PongV1";
+    private GameView mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+
+        enableFullScreen();
+        enableKeepScreanOn();
+        //setContentView(R.layout.activity_game);
+        SGPreferences preferences = getmPreferences();
+        if(preferences.getInt("first_time", -1) == -1){
+            preferences.begin()
+                    .putInt("first_time", 1)
+                    .putInt("difficulty", 0)
+                    .putInt("hight_score", 15)
+                    .end();
+            Log.d(TAG, "first starts");
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Difficulty Level: ");
+        stringBuilder.append(preferences.getInt("difficulty", 0));
+        Log.d(TAG, stringBuilder.toString());
+
+        mView = new GameView(this);
+        setContentView(mView);
     }
 }
